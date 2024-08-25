d822c15d98df1d017f6bfd5449e6c437853dc185
Salah <salah@Salahs-Laptop.local>
2024-08-25 05:40:36 +0300

    first commit

--- src/service/apiMovies.js
+++ src/service/apiMovies.js
@@ -2,13 +2,13 @@ import axios from "axios";
 
 export async function apiMovies(query) {
   const data = await axios({
-    url: `http://www.omdbapi.com/?apikey=53f9eba3&s=${query}`,
+    url: `https://www.omdbapi.com/?apikey=53f9eba3&s=${query}`,
   });
   return data;
 }
 export async function apiMovie(imdbID) {
   const data = await axios({
-    url: `http://www.omdbapi.com/?apikey=53f9eba3&i=${imdbID}`,
+    url: `https://www.omdbapi.com/?apikey=53f9eba3&i=${imdbID}`,
   });
   return data;
 }
